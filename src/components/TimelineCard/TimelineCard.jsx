e02856344818cdb2ab96f143b957b956784e64fd
Arkadiusz Suchenia <suchenia.arkadiusz@aru-software.pl>
2025-02-10 12:14:44 +0100

    Adapt skills, about, header and timeline card to work on phones

--- src/components/TimelineCard/TimelineCard.jsx
+++ src/components/TimelineCard/TimelineCard.jsx
@@ -6,22 +6,25 @@ import firstEduPhoto from "../../assets/edu-photo-1.jpeg";
 import secondEduPhoto from "../../assets/edu-photo-2.jpeg";
 
 export const TimelineCard = ({data, index, isEducation = false}) => {
-  const [cssStyle, setCssStyle] = useState("left")
+  const [image, setImage] = useState(firstExpPhoto);
 
   useEffect(() => {
-    setCssStyle(index % 2 === 0 ? "left" : "right");
-  }, [])
+    if (index % 2 === 0) {
+      setImage(isEducation ? firstEduPhoto : firstExpPhoto);
+    } else {
+      setImage(isEducation ? secondEduPhoto : secondExpPhoto);
+    }
+  }, []);
 
   return (
-    <div className={`timeline-card-container ${cssStyle}`}>
-      {cssStyle === "left" ? (<img src={isEducation ? firstEduPhoto : firstExpPhoto} alt={data.title} className="timeline-img"/>) : null}
-      <div className={`timeline-card-content ${cssStyle}`}>
+    <div className={`timeline-card-container`}>
+      <img src={image} alt={data.title} className="timeline-img"/>
+      <div className={`timeline-card-content`}>
         <h1>{data.company}</h1>
         <h2>{data.position}</h2>
         <h3>{data.startedAt}{data.finishedAt && data.finishedAt.length > 0 ? ` - ${data.finishedAt}` : null}</h3>
         <p>{data.description}</p>
       </div>
-      {cssStyle === "right" ? (<img src={isEducation ? secondEduPhoto : secondExpPhoto} alt={data.title} className="timeline-img"/>) : null}
     </div>
   )
 }
